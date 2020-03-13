3cf2a8dfce62121226a11afc46654e7ddc30ffd3
linnaek <linnaekraemer@gmail.com>
2020-03-12 22:13:01 -0500

    tweak error testing settings

--- tests/unit/test.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils
-import  MainLayout from '../..src/Layout/MainLayout'
-
-describe('Main Layout ', () => {
-  test('includes a header', () => {
-    const wrapper = mount(MainLayout)
-    expect(wrapper.text()).toContain('inLeague')
-  })
-})
